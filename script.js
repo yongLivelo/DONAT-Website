eff2ea73fcc3feab8adb3e6efe6e64ef3f110f21
yongLivelo <94818606+yongLivelo@users.noreply.github.com>
2023-09-12 00:00:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const acronym = document.getElementsByClassName("acronym")
+const acronym_meaning = document.getElementsByClassName("acronym_meaning")
+function showAcronym () {
+acronym_meaning[0].classList.toggle("hidden")
+console.log("hello")
+}
