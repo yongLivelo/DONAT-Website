acfd30ef4759bf5b4872058f27aa0c93a999d3c1
yongLivelo <julioeduardolivelo@gmail.com>
2023-09-12 08:51:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const acronym = document.getElementsByClassName("acronym");
 const acronym_meaning = document.getElementsByClassName("acronym_meaning");
 function showAcronym() {
   acronym_meaning[0].classList.toggle("hidden");
-  console.log("hello1");
+  console.log("hI mawd102");
 }
