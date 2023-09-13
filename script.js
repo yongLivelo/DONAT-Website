84f765671a4038caa22569e007f7891693582c86
yongLivelo <julioeduardolivelo@gmail.com>
2023-09-13 18:38:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const acronym_meaning = document.getElementsByClassName("acronym_meaning");
 function showAcronym() {
   acronym_meaning[0].classList.toggle("hidden");
   console.log("hI mawd102");
+  console.log("niga");
 }
