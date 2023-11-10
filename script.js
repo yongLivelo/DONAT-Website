4ddf5d984795720cc3615ad732954aa4cc5c5d5b
yongLivelo <julioeduardolivelo@gmail.com>
2023-11-10 22:18:59 +0800

    a

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const acronym = document.getElementsByClassName("acronym");
-const acronym_meaning = document.getElementsByClassName("acronym_meaning");
-function showAcronym() {
-  acronym_meaning[0].classList.toggle("hidden");
-}
+"use strict";
+
+const acronym = document.querySelector(".acronym");
+const acronym_meaning = document.querySelector(".acronymm_meaning");
+acronym.addEventListener("mouseenter", (e) => {
+  acronym_meaning.classList.remove("hidden");
+});
+acronym.addEventListener("mouseleave", (e) => {
+  acronym_meaning.classList.add("hidden");
+});
