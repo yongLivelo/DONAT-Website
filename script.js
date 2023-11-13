678f6788017164b2cc5d1607177eb3e7b55f1c06
yongLivelo <julioeduardolivelo@gmail.com>
2023-11-13 23:08:22 +0800

    add tab

--- script.js
+++ script.js
@@ -11,26 +11,27 @@ acronym.addEventListener("mouseenter", (e) => {
 acronym.addEventListener("mouseleave", (e) => {
   acronym_meaning.classList.add("hidden");
 });
-let arr = ["s1", "s2", "s3"];
 
-console.log(arr);
-left_arrow.addEventListener("click", () => {
-  for (let i = 0; i < arr.length; i++) {
-    strands[i].classList.remove(`${arr[i]}`);
-  }
-  const temp = arr.shift();
-  arr.push(temp);
-  for (let i = 0; i < arr.length; i++) {
-    strands[i].classList.add(`${arr[i]}`);
-  }
-});
-right_arrow.addEventListener("click", () => {
-  for (let i = 0; i < arr.length; i++) {
-    strands[i].classList.remove(`${arr[i]}`);
-  }
-  const temp = arr.pop();
-  arr.unshift(temp);
-  for (let i = 0; i < arr.length; i++) {
-    strands[i].classList.add(`${arr[i]}`);
-  }
-});
+// let arr = ["s1", "s2", "s3"];
+
+// console.log(arr);
+// left_arrow.addEventListener("click", () => {
+//   for (let i = 0; i < arr.length; i++) {
+//     strands[i].classList.remove(`${arr[i]}`);
+//   }
+//   const temp = arr.shift();
+//   arr.push(temp);
+//   for (let i = 0; i < arr.length; i++) {
+//     strands[i].classList.add(`${arr[i]}`);
+//   }
+// });
+// right_arrow.addEventListener("click", () => {
+//   for (let i = 0; i < arr.length; i++) {
+//     strands[i].classList.remove(`${arr[i]}`);
+//   }
+//   const temp = arr.pop();
+//   arr.unshift(temp);
+//   for (let i = 0; i < arr.length; i++) {
+//     strands[i].classList.add(`${arr[i]}`);
+//   }
+// });
